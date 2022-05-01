37695139d2f78c85a6ece9bd567f317742b0cc4a
John Gray <john.edwin.gray@gmail.com>
2022-05-01 16:42:40 +1200

    add alt attribute

--- client/components/Fruits.jsx
+++ client/components/Fruits.jsx
@@ -21,7 +21,7 @@ function Fruits () {
             className='flex flex-col align-item-center justify-content-center bg-white rounded-lg overflow-hidden'
           >
             <p className='capitalize color text-neutral-800 text-center'>{fruit.name}</p>
-            <img src={fruit.url} className='h-64 w-64' />
+            <img src={fruit.url} className='h-64 w-64' alt={fruit}/>
           </div>
         ))}
     </div>
